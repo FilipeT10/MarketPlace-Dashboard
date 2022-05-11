b777377cd58a0533098f2d65c6402c8a5814f1a9
filipe.torres <fili.pet81@gmail.com>
2022-05-11 11:17:00 -0300

    Rota de acesso

--- src/services/User.js
+++ src/services/User.js
@@ -5,7 +5,7 @@ class ServiceUser  {
     
    // static getCategorias = () => api.get(`categorias?loja=61663f593ad92700047d5e1f`)
   
-    static authenticate = (json) => api.post(`users/authenticate`, json)
+    static authenticate = (json) => api.post(`users/authenticateSgm`, json)
    /* static saveCategorias = (json) => api.post(`categorias`, json)
   
     static getInfo = () => api.get(`info`)*/
